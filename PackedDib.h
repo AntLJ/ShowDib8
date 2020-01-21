3163f1b39fb72b3ca6404a0fb4145f2466a6d793
AntLJ <51942821+AntLJ@users.noreply.github.com>
2020-01-21 11:08:38 +0800

    自找图片测试

---
+++ PackedDib.h
@@ -0,0 +1,19 @@
+/*-------------------------------------------------------------------------
+ PACKEDIB.H -- Header file for PACKEDIB.C
+ (c) Charles Petzold, 1998
+--------------------------------------------------------------------------*/
+
+BITMAPINFO * PackedDibLoad(PTSTR szFileName);
+int PackedDibGetWidth(BITMAPINFO * pPackedDib);
+int PackedDibGetHeight(BITMAPINFO * pPackedDib);
+int PackedDibGetBitCount(BITMAPINFO * pPackedDib);
+int PackedDibGetRowLength(BITMAPINFO * pPackedDib);
+int PackedDibGetInfoHeaderSize(BITMAPINFO * pPackedDib);
+int PackedDibGetColorsUsed(BITMAPINFO * pPackedDib);
+int PackedDibGetNumColors(BITMAPINFO * pPackedDib);
+int PackedDibGetColorTableSize(BITMAPINFO * pPackedDib);
+RGBQUAD * PackedDibGetColorTablePtr(BITMAPINFO * pPackedDib);
+RGBQUAD * PackedDibGetColorTableEntry(BITMAPINFO * pPackedDib, int i);
+BYTE * PackedDibGetBitsPtr(BITMAPINFO * pPackedDib);
+int PackedDibGetBitsSize(BITMAPINFO * pPackedDib);
+HPALETTE PackedDibCreatePalette(BITMAPINFO * pPackedDib);
